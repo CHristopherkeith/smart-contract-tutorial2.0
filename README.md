3ca92f05dca0bc3a1dad1dc3ff04faeacef3bd76
Christopher <easonmu@hotmail.com>
2018-03-31 19:25:53 +0800

    add wrestling.sol

---
+++ README.md
@@ -0,0 +1,5 @@
+# 智能合约入门教程2.0
+## ethfans上的教程 + 自己的代码实现
+## 教程地址： 
+https://ethfans.org/posts/ethereum-development-walkthrough-part-1-smart-contracts
+https://ethfans.org/posts/ethereum-development-walkthrough-part-2-truffle-ganache-geth-and-mist
---
+++ contracts/Wrestling.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.17;
+
+contract Wrestling {
+    
+    address public wrestler1;
+	address public wrestler2;
+
+	bool public wrestler1Played;
+	bool public wrestler2Played;
+
+	uint private wrestler1Deposit;
+	uint private wrestler2Deposit;
+
+}
