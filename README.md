c17fbc85105baf243c320a4f612922209d2dd584
Christopher <easonmu@hotmail.com>
2018-03-31 20:47:54 +0800

    Truffle + Ganache

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
 # 智能合约入门教程2.0
+
 ## ethfans上的教程 + 自己的代码实现
+
 ## 教程地址： 
 https://ethfans.org/posts/ethereum-development-walkthrough-part-1-smart-contracts
 https://ethfans.org/posts/ethereum-development-walkthrough-part-2-truffle-ganache-geth-and-mist
+
+## 教程说明
+1.Truffle + Ganache
+2.Truffle + Geth + Mist
+
+## 方法说明 
+
+## 玩家地址
+WrestlingInstance.wrestler1.call()
+WrestlingInstance.wrestler2.call()
+## 游戏是否结束
+WrestlingInstance.gameFinished.call()
+## 赢家
+WrestlingInstance.theWinner.call()
+## 提取以太币
+WrestlingInstance.withdraw({from: account1})
+account1必须为赢家地址，若不是提币会回滚
--- contracts/Wrestling.sol
+++ contracts/Wrestling.sol
@@ -11,4 +11,73 @@ contract Wrestling {
 	uint private wrestler1Deposit;
 	uint private wrestler2Deposit;
 
+	bool public gameFinished; 
+	address public theWinner;
+	uint gains;
+
+	event WrestlingStartsEvent(address wrestler1, address wrestler2);
+	event EndOfRoundEvent(uint wrestler1Deposit, uint wrestler2Deposit);
+	event EndOfWrestlingEvent(address winner, uint gains);
+
+	function Wrestling() public {
+	  wrestler1 = msg.sender;
+	}
+
+	function registerAsAnOpponent() public {
+	    require(wrestler2 == address(0));
+
+	    wrestler2 = msg.sender;
+
+	    WrestlingStartsEvent(wrestler1, wrestler2);
+	}
+
+	function wrestle() public payable {
+        require(!gameFinished && (msg.sender == wrestler1 || msg.sender == wrestler2));
+
+        if(msg.sender == wrestler1) {
+            require(wrestler1Played == false);
+            wrestler1Played = true;
+            wrestler1Deposit = wrestler1Deposit + msg.value;
+        } else { 
+            require(wrestler2Played == false);
+            wrestler2Played = true;
+            wrestler2Deposit = wrestler2Deposit + msg.value;
+        }
+        if(wrestler1Played && wrestler2Played) {
+            if(wrestler1Deposit >= wrestler2Deposit * 2) {
+                endOfGame(wrestler1);
+            } else if (wrestler2Deposit >= wrestler1Deposit * 2) {
+                endOfGame(wrestler2);
+            } else {
+                endOfRound();
+            }
+        }
+    }
+
+    function endOfRound() internal {
+        wrestler1Played = false;
+        wrestler2Played = false;
+
+        EndOfRoundEvent(wrestler1Deposit, wrestler2Deposit);
+    }
+
+    function endOfGame(address winner) internal {
+        gameFinished = true;
+        theWinner = winner;
+
+        gains = wrestler1Deposit + wrestler2Deposit;
+        EndOfWrestlingEvent(winner, gains);
+    }
+
+    function withdraw() public {
+        require(gameFinished && theWinner == msg.sender);
+
+        uint amount = gains;
+
+        gains = 0;
+        msg.sender.transfer(amount);
+    }
+
+
+
 }
